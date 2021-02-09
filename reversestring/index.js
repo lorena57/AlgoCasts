d52b8deda9b57a04292d3b85ae0e386c630a5913
Lorena C <lorena@eunknown.org>
2021-02-08 19:55:18 -0800

    inital

---
+++ reversestring/index.js
@@ -0,0 +1,22 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+//split('')
+// ['A', 'B, "C']
+
+//reverse()
+// ['C', 'B, "A"]
+
+//join('') with apostrophe's it removes the space
+//[CBA]
+
+module.exports = reverse;
